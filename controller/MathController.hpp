aaeffebc5e3c0adebcf6e812fc99a37017003a6c
Alexey <savelyev.main@gmail.com>
2024-05-24 14:59:52 +0600

    swagger auth

--- controller/MathController.hpp
+++ controller/MathController.hpp
@@ -23,18 +23,34 @@ public:
 		: oatpp::web::server::api::ApiController(objectMapper)
 	{ }
 
+	ENDPOINT_INFO(sum) {
+		info->tags = std::list<oatpp::String>{ "Math" };
+		info->summary = "Sum of two numbers";
+		info->description = "Complete description of sum";
+
+		info->queryParams["a"].description = "First param of sum";
+	}
 	ENDPOINT("GET", "/math/sum", sum, QUERY(Float32, a, "a"), QUERY(Float32, b, "b")) {
 		return ResponseFactory::createResponse(Status::CODE_200, createDto(a, b, a + b), objectMapper);
 	}
 
+	ENDPOINT_INFO(substract) {
+		info->tags = std::list<oatpp::String>{ "Math" };
+	}
 	ENDPOINT("GET", "/math/substract", substract, QUERY(Float32, a, "a"), QUERY(Float32, b, "b")) {
 		return ResponseFactory::createResponse(Status::CODE_200, createDto(a, b, a - b), objectMapper);
 	}
 
+	ENDPOINT_INFO(multiply) {
+		info->tags = std::list<oatpp::String>{ "Math" };
+	}
 	ENDPOINT("GET", "/math/multiply", multiply, QUERY(Float32, a, "a"), QUERY(Float32, b, "b")) {
 		return ResponseFactory::createResponse(Status::CODE_200, createDto(a, b, a * b), objectMapper);
 	}
 
+	ENDPOINT_INFO(divide) {
+		info->tags = std::list<oatpp::String>{ "Math" };
+	}
 	ENDPOINT("GET", "/math/divide", divide, QUERY(Float32, a, "a"), QUERY(Float32, b, "b")) {
 		return ResponseFactory::createResponse(Status::CODE_200, createDto(a, b, a / b), objectMapper);
 	}
--- controller/TodoController.hpp
+++ controller/TodoController.hpp
@@ -10,29 +10,74 @@ class TodoController : public oatpp::web::server::api::ApiController {
 private:
 	OATPP_COMPONENT(std::shared_ptr<oatpp::data::mapping::ObjectMapper>, objectMapper);
 	List<Object<TodoDto>> todos = List<Object<TodoDto>>::createShared();
+
+	// функция для проверки данных авторизации
+	void checkAuth(std::shared_ptr<oatpp::web::server::handler::DefaultBasicAuthorizationObject> authObject) {
+		if (authObject->userId == "admin" && authObject->password == "admin") {
+			return;
+		}
+		else {
+			throw oatpp::web::protocol::http::HttpError(oatpp::web::protocol::http::Status::CODE_401, "Unauthorized", {});
+		}
+	}
 public:
 	TodoController(OATPP_COMPONENT(std::shared_ptr<ObjectMapper>, objectMapper))
 		: oatpp::web::server::api::ApiController(objectMapper)
-	{ }
-
-	// получить список всех заметок
-	ENDPOINT("GET", "todo/all", todosAll) {
-		return ResponseFactory::createResponse(Status::CODE_200, todos, objectMapper);
+	{ 
+		setDefaultAuthorizationHandler(std::make_shared<oatpp::web::server::handler::BasicAuthorizationHandler>("auth-handler"));
 	}
 
-	ENDPOINT("GET", "todo/{id}", todosOne, PATH(Int16, id)) {
+	ENDPOINT_INFO(todosAll) {
+		info->tags = std::list<oatpp::String>{ "Todo" };
+		info->addSecurityRequirement("Authorization required");
+	}
+	// получить список всех заметок
+	ENDPOINT("GET", "todo/all", todosAll,
+		AUTHORIZATION(std::shared_ptr<oatpp::web::server::handler::DefaultBasicAuthorizationObject>, authObject)) {
+		checkAuth(authObject);
+		return ResponseFactory::createResponse(Status::CODE_200, todos, objectMapper);	
+	}
+
+	ENDPOINT("GET", "/todo/about", todosAbout) {
+		std::string header = "<h1>Todo Controller</h1>";
+		std::string list = "<ul><li>Create todos</li><li>Update todos></li><li>Delete todos</li></ul>";
+
+		return ResponseFactory::createResponse(Status::CODE_200, header + list);
+	}
+
+	ENDPOINT_INFO(todosOne) {
+		info->tags = std::list<oatpp::String>{ "Todo" };
+		info->addSecurityRequirement("Authorization required");
+	}
+	ENDPOINT("GET", "todo/{id}", todosOne, PATH(Int16, id),
+		AUTHORIZATION(std::shared_ptr<oatpp::web::server::handler::DefaultBasicAuthorizationObject>, authObject)) {
+		checkAuth(authObject);
 		if (id >= todos->size()) {
 			return ResponseFactory::createResponse(Status::CODE_404, "Заметка не найдена");
 		}
 		return ResponseFactory::createResponse(Status::CODE_200, todos, objectMapper);
 	}
 
-	ENDPOINT("POST", "/todo", todosAdd, BODY_DTO(Object<TodoDto>, todoDto)) {
+	ENDPOINT_INFO(todosAdd) {
+		info->tags = std::list<oatpp::String>{ "Todo" };
+		info->addSecurityRequirement("Authorization required");
+	}
+	ENDPOINT("POST", "/todo", todosAdd, BODY_DTO(Object<TodoDto>, todoDto),
+		AUTHORIZATION(std::shared_ptr<oatpp::web::server::handler::DefaultBasicAuthorizationObject>, authObject)) {
+		checkAuth(authObject);
 		todos->push_back(todoDto);
 		return ResponseFactory::createResponse(Status::CODE_201, todoDto, objectMapper);
 	}
 
-	ENDPOINT("PUT", "/todo/{id}", todosUpdate, BODY_DTO(Object<TodoDto>, todoDto), PATH(Int16, id)) {
+
+	ENDPOINT_INFO(todosUpdate) {
+		info->tags = std::list<oatpp::String>{ "Todo" };
+		info->pathParams["id"].description = "Description of ID";
+		info->addSecurityRequirement("Authorization required");
+	}
+	ENDPOINT("PUT", "/todo/{id}", todosUpdate, BODY_DTO(Object<TodoDto>, todoDto), PATH(Int16, id),
+		AUTHORIZATION(std::shared_ptr<oatpp::web::server::handler::DefaultBasicAuthorizationObject>, authObject)) {
+		checkAuth(authObject);
 		if (id >= todos->size()) {
 			return ResponseFactory::createResponse(Status::CODE_404, "Заметка не найдена");
 		}
@@ -42,7 +87,13 @@ public:
 		return ResponseFactory::createResponse(Status::CODE_200, todoDto, objectMapper);
 	}
 
-	ENDPOINT("DELETE", "/todo/{id}", todosDelete, PATH(Int16, id)) {
+	ENDPOINT_INFO(todosDelete) {
+		info->tags = std::list<oatpp::String>{ "Todo" };
+		info->addSecurityRequirement("Authorization required");
+	}
+	ENDPOINT("DELETE", "/todo/{id}", todosDelete, PATH(Int16, id),
+		AUTHORIZATION(std::shared_ptr<oatpp::web::server::handler::DefaultBasicAuthorizationObject>, authObject)) {
+		checkAuth(authObject);
 		if (id >= todos->size()) {
 			return ResponseFactory::createResponse(Status::CODE_404, "Заметка не найдена");
 		}
@@ -51,6 +102,7 @@ public:
 
 		return ResponseFactory::createResponse(Status::CODE_204, "");
 	}
+
 };
 
 #include OATPP_CODEGEN_END(ApiController)
